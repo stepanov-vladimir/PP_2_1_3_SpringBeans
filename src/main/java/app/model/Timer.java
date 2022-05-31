fea2c2cadd3c898588133ed246a88c4723c71c82
Vladimir Stepanov <niachem@gmail.com>
2022-05-31 23:35:42 +0300

    complete task 2.1.3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,15 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("timerBean")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
+    @Autowired
     public Long getTime() {
         return nanoTime;
     }
